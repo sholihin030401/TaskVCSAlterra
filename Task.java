74a1e5ff75c433c24c70b78ed38846a1e5e74777
Ichwan Sholihin <ichwansholihin03@gmail.com>
2022-02-15 16:55:39 +0700

    Fixing File Task

--- Task.java
+++ Task.java
@@ -1,5 +1,9 @@
 public class Task{
     public static void main(String[] args){
         System.out.println("Hello World");
+        System.out.println("Hello, A");
+        System.out.println("Hello, B");
+        System.out.println("Hello, C");
+        System.out.println("Hello, D");
     }
 }
